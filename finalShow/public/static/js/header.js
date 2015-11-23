68e125b02a1810252983f270325e78ae3cab4e8c
Kinice <szp93@126.com>
2015-11-23 19:21:03 +0800

    ..

--- finalShow/public/static/js/header.js
+++ finalShow/public/static/js/header.js
@@ -1,31 +1,31 @@
-/*$(function(){
+$(function(){
 	var timeout         = 100;
 	var closetimer		= 0;
 	var ddmenuitem      = 0;
 
-	function jsddm_open(){	
-		jsddm_canceltimer();
-		jsddm_close();
+	function open(){
+		canceltimer();
+		close();
 		ddmenuitem = $(this).find('ul').eq(0).css('visibility', 'visible');
 	}
 
-	function jsddm_close(){	
+	function close(){
 		if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');
 	}
 
-	function jsddm_timer(){	
-		closetimer = window.setTimeout(jsddm_close, timeout);
+	function timer(){
+		closetimer = window.setTimeout(close, timeout);
 	}
-	function jsddm_canceltimer(){	
+	function canceltimer(){
 		if(closetimer){	
 			window.clearTimeout(closetimer);
 			closetimer = null;
 		}
 	}
 	$(document).ready(function(){	
-		$('.nav-inside').bind('mouseover', jsddm_open);
-		$('.nav-inside').bind('mouseout',  jsddm_timer);
+		$('.nav-inside').bind('mouseover', open);
+		$('.nav-inside').bind('mouseout',  timer);
 	});
-	document.onclick = jsddm_close;
-});*/
+	document.onclick = close;
+});
 
