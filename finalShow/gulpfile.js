5bf1a499a25f453df0f55d1979e7709a5373f436
Kinice <szp93@126.com>
2016-01-14 14:21:37 +0800

    重构css，修改gulp

--- finalShow/gulpfile.js
+++ finalShow/gulpfile.js
@@ -1,8 +1,38 @@
 var gulp = require('gulp'),
-   uglify = require('gulp-uglify');
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create(),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    reload = browserSync.reload;
 
-gulp.task('minify', function () {
-   gulp.src('js/app.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('build'))
+
+gulp.task('less', function(){
+    gulp.src('/public/less/*.less')
+        .pipe(less())
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/static/css'))
+        .pipe(reload({stream:true}));
+});
+gulp.task('script', function(){
+    gulp.src('public/js/*.js')
+        .pipe(rename({suffix:'.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/static/js'));
+});
+gulp.task('browser-sync',['script','less'],function(){
+    browserSync.init({
+        proxy: 'http://localhost:3000',
+        baseDir: './',
+        port: 3000,
+        uiPort: 3000
+    });
+    gulp.watch('public/less/*.less',['less']);
+    gulp.watch('public/js/*.js',['script']);
+    gulp.watch('public/static/css/*.css').on('change',reload);
+    gulp.watch('public/static/js/*.js').on('change',reload);
+    gulp.watch('views/*.ejs').on('change',reload);
+});
+gulp.task('default',['browser-sync'], function(){
+    console.log('Mission Complete');
 });
--- finalShow/public/js/header.js
+++ finalShow/public/js/header.js
@@ -1,33 +1,3 @@
-/*$(function(){
-	var timeout         = 100;
-	var closetimer		= 0;
-	var ddmenuitem      = 0;
-
-	function open(){
-		canceltimer();
-		close();
-		ddmenuitem = $(this).find('ul').eq(0).css('visibility', 'visible');
-	}
-
-	function close(){
-		if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');
-	}
-
-	function timer(){
-		closetimer = window.setTimeout(close, timeout);
-	}
-	function canceltimer(){
-		if(closetimer){	
-			window.clearTimeout(closetimer);
-			closetimer = null;
-		}
-	}
-	$(document).ready(function(){	
-		$('.nav-inside').bind('mouseover', open);
-		$('.nav-inside').bind('mouseout',  timer);
-	});
-	document.onclick = close;
-});*/
 $(function(){
 	$('.menu').click(function() {
 		/* Act on the event */
