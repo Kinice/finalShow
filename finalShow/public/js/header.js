5011adbe3bd0363c2c0d6f4fb44fbd3d5fce8eb5
Kinice <szp93@126.com>
2016-04-19 18:59:04 +0800

    样式更新＋js封装

--- finalShow/public/js/header.js
+++ finalShow/public/js/header.js
@@ -1,48 +1,8 @@
-$(function(){
-    var $pin = $('.pin');
-    var $hd = $('header');
-    if(sessionStorage.pin == 'fixed'){
-        dispFixed($hd);
-        $pin.css({
-            'transform':'rotate(0deg)',
-            'color':'#f26651'
-        });
-    }else{
-        dispAbsolute($hd);
-        $pin.css({
-            'transform':'rotate(-90deg)',
-            'color':'#fff'
-        });
-    }
-	$('.menu').click(function() {
-		/* Act on the event */
-//        var randNum = Math.random();
-//        if(randNum >= 0 && randNum <0.5){
-//            menuSlide();
-//        }else{
-//            menuFade();
-//        }
-        menuSlide();
-	});
-//    $('.home').click(function(){
-//        backOne();
-//    });
-    $(document).scroll(function(){
-        if(getScrollTop() <= 600){
-            $('.back-to-top').stop().fadeOut();
-        }else if(getScrollTop() >= 600){
-            //Back To Top
-            $('.back-to-top').stop().fadeIn();
-        }
-    });
-    $('.back-to-top').click(function(){
-        $('html,body').animate({
-            scrollTop: 0
-        },500);
-    });
-    $('.pin').click(function() {
-        /* Act on the event */
-        if($hd.css('position')=='absolute'){
+(function(){
+    $(function(){
+        var $pin = $('.pin');
+        var $hd = $('header');
+        if(sessionStorage.pin == 'fixed'){
             dispFixed($hd);
             $pin.css({
                 'transform':'rotate(0deg)',
@@ -55,16 +15,57 @@ $(function(){
                 'color':'#fff'
             });
         }
+        $('.menu').click(function() {
+            /* Act on the event */
+//        var randNum = Math.random();
+//        if(randNum >= 0 && randNum <0.5){
+//            menuSlide();
+//        }else{
+//            menuFade();
+//        }
+            menuSlide();
+        });
+//    $('.home').click(function(){
+//        backOne();
+//    });
+        $(document).scroll(function(){
+            if(getScrollTop() <= 600){
+                $('.back-to-top').stop().fadeOut();
+            }else if(getScrollTop() >= 600){
+                //Back To Top
+                $('.back-to-top').stop().fadeIn();
+            }
+        });
+        $('.back-to-top').click(function(){
+            $('html,body').animate({
+                scrollTop: 0
+            },500);
+        });
+        $('.pin').click(function() {
+            /* Act on the event */
+            if($hd.css('position')=='absolute'){
+                dispFixed($hd);
+                $pin.css({
+                    'transform':'rotate(0deg)',
+                    'color':'#f26651'
+                });
+            }else{
+                dispAbsolute($hd);
+                $pin.css({
+                    'transform':'rotate(-90deg)',
+                    'color':'#fff'
+                });
+            }
+        });
     });
-});
 
-function menuSlide(){
-    if($('.mobile-menu').css('display') == 'block'){
-        $('.mobile-menu').slideUp(150);
-    }else{
-        $('.mobile-menu').slideDown(150);
+    function menuSlide(){
+        if($('.mobile-menu').css('display') == 'block'){
+            $('.mobile-menu').slideUp(150);
+        }else{
+            $('.mobile-menu').slideDown(150);
+        }
     }
-}
 //function menuFade(){
 //    if($('.mobile-menu').css('display') == 'block'){
 //        $('.mobile-menu').fadeOut(150);
@@ -72,21 +73,22 @@ function menuSlide(){
 //        $('.mobile-menu').fadeIn(150);
 //    }
 //}
-function getScrollTop(){
-    return $(document).scrollTop();
-}
+    function getScrollTop(){
+        return $(document).scrollTop();
+    }
 //function backOne(){
 //    window.history.go(-1);
 //}
-function dispAbsolute(element){
-    sessionStorage.pin = 'absolute';
-    element.css('position','absolute');
-}
-function dispFixed(element){
-    sessionStorage.pin = 'fixed';
-    element.css('position','fixed');
-}
+    function dispAbsolute(element){
+        sessionStorage.pin = 'absolute';
+        element.css('position','absolute');
+    }
+    function dispFixed(element){
+        sessionStorage.pin = 'fixed';
+        element.css('position','fixed');
+    }
 
+}());
 (function(){
     var oriTitle = document.title;
     var hidden,state,visibilityChange;
