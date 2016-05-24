acaf7ef4fb2086db5c9ced1808d0257b6c59f7c3
kinice <szp93@126.com>
2016-05-24 09:57:37 +0800

    bug

--- finalShow/routes/index.js
+++ finalShow/routes/index.js
@@ -274,6 +274,9 @@ module.exports = function(app) {
                 req.flash('error', err);
                 return res.redirect('/');
             }
+            res.setHeader("Access-Control-Allow-Origin","*");
+            res.setHeader("Access-Control-Allow-Headers","Content-Type,Accept,Authorization");
+            res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,UPDATE,DELETE");
             res.jsonp(post);
         });
     });
