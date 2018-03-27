b08dc9084d59c633e0a869290d6f3fc20f65d26a
Kinice <szp93@126.com>
2018-03-27 16:34:51 +0800

    Add connect & disconnect function.

--- models/socket.js
+++ models/socket.js
@@ -5,13 +5,24 @@ var socket_io = require('socket.io');
 socketio.getSocketio = function(server){  
     var io = socket_io.listen(server);
     io.on('connection', function(socket){
+        var userInfo = {
+            id: socket.id,
+            username: ''
+        }
         console.log('a user connected');
+        socket.on('user connect', function(msg) {
+            io.emit('user connect', msg)
+            userInfo.username = msg.username
+        })
         socket.on('socket message', function(msg) {
             console.log(msg);
             io.emit('socket message', msg);
         })
         socket.on('disconnect', function(){
-            console.log('user disconnected');
+            console.log('user' + userInfo.username +' disconnected');
+            io.emit('user disconnect', {
+                username: userInfo.username
+            })
         });
     });
 };  
